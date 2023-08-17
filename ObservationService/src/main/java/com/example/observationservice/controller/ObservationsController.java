4f8b7e5aca72bcec6d401eda545f8825d1921622
Lipinski Vincent <lipinskivincent@gmail.com>
2023-08-17 16:19:45 +0200

    security

--- ObservationService/src/main/java/com/example/observationservice/controller/ObservationsController.java
+++ ObservationService/src/main/java/com/example/observationservice/controller/ObservationsController.java
@@ -4,6 +4,7 @@ import com.example.observationservice.dto.ObservationsResponseDTO;
 import com.example.observationservice.entity.Observations;
 import com.example.observationservice.service.ObservationsService;
 import com.example.observationservice.tool.RestClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,16 +20,20 @@ public class ObservationsController {
         this.observationsService = observationsService;
     }
 
+//    @PostMapping("") sans security
+//    public ResponseEntity<Observations> post(@RequestParam int userId,@RequestParam int celestialObjectId,@RequestParam Date date,@RequestParam String description,@RequestParam String urlPicture) {
+//        Observations observations = observationsService.createObservations(userId,celestialObjectId,description,date,urlPicture );
+//        return ResponseEntity.ok(observations);
+//    }
+
     @PostMapping("")
-    public ResponseEntity<Observations> post(@RequestParam int userId,@RequestParam int celestialObjectId,@RequestParam Date date,@RequestParam String description,@RequestParam String urlPicture) {
-//        RestClient<String, String> restClient = new RestClient<>();
-//        if(restClient.testToken(token, String.class)) {
-//            Order order = orderService.createOrder(userId, productName);
-//            return ResponseEntity.ok(order);
-//        }
-//        return ResponseEntity.status(401).body(null);
-        Observations observations = observationsService.createObservations(userId,celestialObjectId,description,date,urlPicture );
-        return ResponseEntity.ok(observations);
+    public ResponseEntity<Observations> post(@RequestParam int userId,@RequestParam int celestialObjectId,@RequestParam Date date,@RequestParam String description,@RequestParam String urlPicture, @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        RestClient<String, String> restClient = new RestClient<>();
+        if(restClient.testToken(token, String.class)) {
+            Observations observations = observationsService.createObservations(userId,celestialObjectId,description,date,urlPicture );
+            return ResponseEntity.ok(observations);
+        }
+        return ResponseEntity.status(401).body(null);
     }
 
     @GetMapping("user/{userId}")
